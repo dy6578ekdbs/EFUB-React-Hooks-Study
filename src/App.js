c50d85db14feb51398a6703926e19b4f42326177
jiwoorld <bncmdbev@gmail.com>
2023-03-21 15:12:29 +0900

    1.3 useTab

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import { ReactDOM } from "react";
+import HookuseTab from "./HookuseTab";
 import HookuseInput2 from "./HookuseInput2";
 import HookuseState from "./HookuseState";
 
 function App() {
   return (
-    <HookuseInput2></HookuseInput2>
+    <HookuseTab></HookuseTab>
   );
 }
 
---
+++ src/HookuseTab.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import React, { useState } from "react";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return { currentItem: allTabs[currentIndex], changeItem:setCurrentIndex };
+};
+
+function HookuseTab() {
+  const { currentItem, changeItem } = useTabs(0, content);
+  return (
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
+    </div>
+  );
+}
+
+export default HookuseTab;
