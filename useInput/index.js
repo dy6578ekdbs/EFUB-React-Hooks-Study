18cd061f7b5a14d387890ebde88ca1f0a83f8d8e
Hanby Choi <hanby.choi@ewhain.net>
2023-03-24 16:50:48 +0900

    [실전형 리액트 Hooks] #2.1 useTitle 실습 코드

--- useInput/index.js
+++ useInput/index.js
@@ -1,16 +1,13 @@
 import React, { useState, StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import { useTitle } from "./useTitle";
 
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number+1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber+1)}>{aNumber}</button>
     </div>
   );
 };
