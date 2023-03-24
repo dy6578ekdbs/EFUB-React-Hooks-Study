fbf4b0b4366a6b527e88bed8cd57c7b2d9c7f326
Hanby Choi <hanby.choi@ewhain.net>
2023-03-24 16:04:57 +0900

    [실전형 리액트 Hooks] #1.1-1.2 useInput 실습 코드

---
+++ useInput/index.js
@@ -0,0 +1,23 @@
+import React, { useState, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { useInput } from "./useInput";
+
+const App = () => {
+    const maxLen = (value) => value.length <= 10; // !value.includes("@")
+    const name = useInput("Mr.", maxLen);
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <input placeholder="Name" {...name} />
+      </div>
+    );
+};
+  
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+  
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
