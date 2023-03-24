55c366c555d62f640b239d51a1637d419a1431a6
songing01 <songing01@gmail.com>
2023-03-25 02:57:00 +0900

    useTitle

---
+++ useTitle.js
@@ -0,0 +1,26 @@
+import ReactDOM from "react-dom";
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
