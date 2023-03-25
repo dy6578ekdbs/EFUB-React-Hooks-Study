076f8c1b587e2ed387a993645d5267115d221691
wowalswjd <alswjd5491@naver.com>
2023-03-25 21:43:53 +0900

    1.3 useTabs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import HookuseInput from "./hooks/HookuseInput";
 import HookuseInput2 from "./hooks/HookuseInput2";
 import HookuseState from "./hooks/HookuseState";
+import HookuseTab from "./hooks/HookuseTab";
 
 function App() {
   return (
     // <HookuseState />
     // <HookuseInput />
-    <HookuseInput2 />
+    // <HookuseInput2 />
+    <HookuseTab />
   );
 }
 
