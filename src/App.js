0dbcf02fb9c1d811e75fc6742bc93a1955ae89bf
wowalswjd <alswjd5491@naver.com>
2023-03-25 22:12:05 +0900

    2.1 useTitle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HookuseInput from "./hooks/HookuseInput";
 import HookuseInput2 from "./hooks/HookuseInput2";
 import HookuseState from "./hooks/HookuseState";
 import HookuseTab from "./hooks/HookuseTab";
+import HookuseTitle from "./hooks/HookuseTitle";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
     // <HookuseInput />
     // <HookuseInput2 />
     // <HookuseTab />
-    <HookuseEffect />
+    // <HookuseEffect />
+    <HookuseTitle />
   );
 }
 
