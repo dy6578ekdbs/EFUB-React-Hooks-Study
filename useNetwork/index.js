66aa49e7da233a170b052da47473d050f732784e
Hanby Choi <hanby.choi@ewhain.net>
2023-04-07 14:16:59 +0900

    [실전형 리액트 Hooks] 2.5 useFadeIn, useNetwork 실습 코드

---
+++ useNetwork/index.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, StrictMode, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { useNetwork } from "./useNetwork";
+
+const App = () => {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div className="App">
+      <h1>{onLine ? "Online" : "Offline"}</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
