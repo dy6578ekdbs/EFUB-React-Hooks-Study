fa49bcc673410da6d106f847ac5b092b41889275
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-04-08 23:52:12 +0900

    ✨ feat: useScroll

---
+++ useScroll/useScroll.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+/**
+ *
+ * @returns 스크롤 x, y 위치
+ */
+export const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  return state;
+};
