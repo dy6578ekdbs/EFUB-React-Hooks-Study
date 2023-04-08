4536b7ba8bfc83a00a14024f15fb036363e9bb2d
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-04-08 23:52:23 +0900

    ✨ feat: useNetwork

---
+++ useNetwork/useNetwork.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine || true);
+
+  const handleChange = () => {
+    if (onChange && typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+  return status;
+};
